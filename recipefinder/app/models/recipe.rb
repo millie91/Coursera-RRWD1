9e4dfe510acad5b913f5945ef7799fff4f825233
Milica Sucevic <milicasucevic@gmail.com>
2017-01-22 15:38:18 +0000

    Code Updated

--- recipefinder/app/models/recipe.rb
+++ recipefinder/app/models/recipe.rb
@@ -1,16 +1,19 @@
 require 'httparty'
+require 'pp'
 
-class Recipe 
-  include HTTParty
+class Recipe
+	include HTTParty
+
+
+	default_options.update(verify: false) # Turn off SSL verification
   
-  format :json
+  	key_value = ENV['FOOD2FORK_KEY'] || "f2905eac941284002fa65126412ca9d6"
+	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
+  	base_uri "http://#{hostport}/api"
+  	default_params key: key_value
+  	format :json
 
-  key_value = ENV["FOOD2FORK_KEY"]
-  default_params key: ENV["FOOD2FORK_KEY"]
-  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || "www.food2fork.com"
-  base_uri "http://#{hostport}/api"
-
-  def self.for (keyword)
-    get("/search", query: {q: keyword})["recipes"]
-  end
+  	def self.for (keyword)
+   		get("/search", query: {q: keyword})["recipes"]
+  	end
 end
