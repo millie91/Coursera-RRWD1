207d665492ff14b28d05d3016e110b22c951d201
Milica Sucevic <milicasucevic@gmail.com>
2017-01-22 17:32:19 +0000

    Code Updated

--- recipefinder/app/models/recipe.rb
+++ recipefinder/app/models/recipe.rb
@@ -1,12 +1,17 @@
-class Recipe
-  
-  	key_value = ENV['FOOD2FORK_KEY']
-	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
-  	base_uri "http://#{hostport}/api"
-  	default_params key: "f2905eac941284002fa65126412ca9d6"
-  	format :json
+require 'httparty'
+require 'htmlentities'
+HtmlEntities.new.decode("Kahl&uacute;a-Spiked") # "Kahlúa-Spiked"
 
-  	def self.for (keyword)
-   		get("/search", query: {q: keyword})["recipes"]
-  	end
+class Recipe
+  include HTTParty
+
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
